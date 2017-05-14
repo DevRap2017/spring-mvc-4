2e9c116c3a834993930ff4c2cd95fed445eea82d
HASSAN ABANE <hassan.abane@gmail.com>
2017-05-14 17:21:00 +0100

    Spring MVC 4 with WEB.XML

---
+++ hello-with-web-xml/src/main/java/com/devrap/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.devrap.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/hello")
+	public String sayHello(Model model) {
+		model.addAttribute("greeting", "Hello HASSAN");
+		return "index.jsp";
+	}
+
+}
